59e50236c6b6fda95a4386631c7806450be18e32
hudadamar21 <hudadamar21@gmail.com>
2023-05-17 19:58:58 +0700

    create IconDot

---
+++ src/components/icons/IconDot.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle
+      cx="8.5"
+      cy="8.5"
+      r="8.5"
+      transform="rotate(180 8.5 8.5)"
+      fill="white"
+      fill-opacity="0.5"
+    />
+    <circle cx="8.5" cy="8.5" r="4.5" transform="rotate(180 8.5 8.5)" fill="white" />
+  </svg>
+</template>
