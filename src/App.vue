b6ff046fc5d69d1fdd39ce662b5c5f07c0cfb928
hudadamar21 <hudadamar21@gmail.com>
2023-05-17 18:38:08 +0700

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="main-container flex items-center justify-between py-16">
+    <nav class="flex items-center gap-x-16">
+      <RouterLink to="/"><img src="/images/logo.svg" alt="logo" /></RouterLink>
+      <RouterLink to="/overview" class="font-medium tracking-wide text-white/60 hover:text-white transition">Overview</RouterLink>
+      <RouterLink to="/skills" class="font-medium tracking-wide text-white/60 hover:text-white transition">Skills</RouterLink>
+      <RouterLink to="/projects" class="font-medium tracking-wide text-white/60 hover:text-white transition">Projects</RouterLink>
+      <RouterLink to="/contact" class="font-medium tracking-wide text-white/60 hover:text-white transition">Contact</RouterLink>
+    </nav>
+    <RouterLink to="/contact" class="bg-primary-green px-8 py-3.5 rounded-md">
+      Punya Project?
+    </RouterLink>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
