8e9707544ff1fd054f9b2b425dcb320726b6adbb
hudadamar21 <hudadamar21@gmail.com>
2023-05-17 17:56:20 +0700

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    extend: {
+      colors: {
+        dark: '#1E1F2B',
+        primary: {
+          green: '#2BA386'
+        }
+      },
+    }
   },
   plugins: []
 }
