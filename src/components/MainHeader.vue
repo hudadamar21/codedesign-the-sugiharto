6bd46366c31c00a4b1afd63373957b5846392877
hudadamar21 <hudadamar21@gmail.com>
2023-05-17 21:22:55 +0700

    header: responsive mobile

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,34 +1,69 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+import IconMenu from './icons/IconMenu.vue'
+
+const open = ref(false)
 </script>
 
 <template>
-  <header class="main-container flex items-center justify-between py-16">
-    <nav class="flex items-center gap-x-16">
-      <RouterLink to="/"><img src="/images/logo.svg" alt="logo" /></RouterLink>
-      <RouterLink
-        to="/overview"
-        class="font-medium tracking-wide text-white/60 hover:text-white transition"
-        >Overview</RouterLink
+  <header class="main-container flex items-center justify-between py-8 md:py-16">
+    <div class="flex items-center gap-x-8 md:gap-x-16">
+      <RouterLink @click="open = false" class="relative z-30 flex items-center gap-x-3" to="/">
+        <img class="w-8 lg:w-14 h-8 lg:h-14" src="/images/logo.svg" alt="logo" />
+        <p class="block l font-semibold mt-0.5 tracking-widest text-sm">The Sugiharto</p>
+      </RouterLink>
+      <nav
+        class="absolute lg:static top-0 left-0 flex flex-col lg:flex-row w-full pt-28 pb-12 space-y-8 lg:space-y-0 lg:py-0 bg-dark z-20 items-center gap-x-8 md:gap-x-16 transition-transform duration-500"
+        :class="!open ? '-translate-y-[150%] lg:translate-y-0' : 'translate-y-0'"
       >
+        <RouterLink
+          @click="open = false"
+          to="/overview"
+          class="font-medium tracking-wide text-white/60 hover:text-white transition"
+          >Overview</RouterLink
+        >
+        <RouterLink
+          @click="open = false"
+          to="/skills"
+          class="font-medium tracking-wide text-white/60 hover:text-white transition"
+          >Skills</RouterLink
+        >
+        <RouterLink
+          @click="open = false"
+          to="/projects"
+          class="font-medium tracking-wide text-white/60 hover:text-white transition"
+          >Projects</RouterLink
+        >
+        <RouterLink
+          @click="open = false"
+          to="/contact"
+          class="font-medium tracking-wide text-white/60 hover:text-white transition"
+          >Contact</RouterLink
+        >
+        <RouterLink
+          @click="open = false"
+          to="/contact"
+          class="block lg:hidden relative z-30 bg-primary-green text-sm lg:text-base px-4 lg:px-8 py-2 lg:py-3 rounded-md"
+        >
+          Punya Project?
+        </RouterLink>
+      </nav>
+    </div>
+    <div class="flex items-center gap-x-5">
       <RouterLink
-        to="/skills"
-        class="font-medium tracking-wide text-white/60 hover:text-white transition"
-        >Skills</RouterLink
-      >
-      <RouterLink
-        to="/projects"
-        class="font-medium tracking-wide text-white/60 hover:text-white transition"
-        >Projects</RouterLink
-      >
-      <RouterLink
+        @click="open = false"
         to="/contact"
-        class="font-medium tracking-wide text-white/60 hover:text-white transition"
-        >Contact</RouterLink
+        class="hidden lg:block relative z-30 bg-primary-green text-sm lg:text-base px-4 lg:px-8 py-2 lg:py-3 rounded-md"
       >
-    </nav>
-    <RouterLink to="/contact" class="bg-primary-green px-8 py-3.5 rounded-md">
-      Punya Project?
-    </RouterLink>
+        Punya Project?
+      </RouterLink>
+      <button
+        class="relative z-30 hover:opacity-80 transition lg:hidden mt-1"
+        @click="open = !open"
+      >
+        <IconMenu />
+      </button>
+    </div>
   </header>
 </template>
---
+++ src/components/icons/IconMenu.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256">
+    <path
+      fill="currentColor"
+      d="M224 120v16a8 8 0 0 1-8 8H40a8 8 0 0 1-8-8v-16a8 8 0 0 1 8-8h176a8 8 0 0 1 8 8Zm-8 56H40a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h176a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8Zm0-128H40a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h176a8 8 0 0 0 8-8V56a8 8 0 0 0-8-8Z"
+    />
+  </svg>
+</template>
