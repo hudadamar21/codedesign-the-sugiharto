ec79184ac2447d89dd979bf5e67ce01638a7551b
hudadamar21 <hudadamar21@gmail.com>
2023-05-17 19:59:10 +0700

    create overview data

---
+++ src/data/overview.js
@@ -0,0 +1,27 @@
+export const overview = {
+  description:
+    'Nama saya Dedik Sugiharto, saya seorang software-engineer sebelum kata software-engineer itu ada. Saya terbiasa ngoding di Microsoft Word 2003.',
+  photo: {
+    url: '/images/photo.png',
+    caption: 'Foto diambil 2018'
+  },
+  bio: [
+    {
+      name: 'Pendidikan',
+      list: [
+        '2003-2009 SDN California 2',
+        '2009-2012 SMP Los Santos 9',
+        '2012-2015 SMA Berlin Jakarta 64'
+      ]
+    },
+    {
+      name: 'Pekerjaan',
+      list: [
+        'Software Engineer at PT. Mencari Cinta Sejati',
+        'Co-founder at A.D.M (Artis Dunia Maya)',
+        'Manager at Liverpool F.C',
+        'Manager at Cliquers Cllamanya'
+      ]
+    }
+  ]
+}
