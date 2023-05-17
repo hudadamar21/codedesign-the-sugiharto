6e8da24eb11396f589ca8a159df1dd18c2e0fca1
hudadamar21 <hudadamar21@gmail.com>
2023-05-17 18:39:33 +0700

    init all pages

---
+++ src/views/HomePage.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main class="main-container flex items-center justify-between pt-20">
+    ok
+  </main>
+</template>
---
+++ src/views/OverviewPage.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main class="bg-blue-500 h-screen">
+    ok
+  </main>
+</template>
