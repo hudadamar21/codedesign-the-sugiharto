f5b9d79a04c60fa9d3bd72a4bc60403e8fde770c
hudadamar21 <hudadamar21@gmail.com>
2023-05-17 17:54:05 +0700

    install tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="bg-blue-500 h-screen">
+    ok
   </main>
 </template>
