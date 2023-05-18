f52d674801e550e7ed3c0ea4740ce8be9f7da555
hudadamar21 <hudadamar21@gmail.com>
2023-05-18 17:30:19 +0700

    create contact data

---
+++ src/data/contact.js
@@ -0,0 +1,16 @@
+export const contact = {
+  description: 'Kamu bisa menghubungi saya lewat kedua kontak di bawah ini.',
+  quote: 'Aku akan sangat senang sekali mendapat pesan dari Anda',
+  list: [
+    {
+      name: 'Phone',
+      body: '+62 1234 8921',
+      icon: '/icons/phone.svg'
+    },
+    {
+      name: 'Email',
+      body: 'support@collosal.tld',
+      icon: '/icons/email.svg'
+    }
+  ]
+}
