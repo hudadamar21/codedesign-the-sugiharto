33a559237b65d6702338b4a5cc068e577d06ca9f
hudadamar21 <hudadamar21@gmail.com>
2023-05-18 16:40:12 +0700

    complete skills page

--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -1,8 +1,21 @@
 <script setup>
+import SectionTitle from '../components/SectionTitle.vue'
+import CardNormal from '../components/CardNormal.vue'
+import { skills } from '../data/skills'
 </script>
 
 <template>
-  <main class="bg-blue-500 h-screen">
-    ok
+  <main class="relative main-container pt-8 pb-28">
+    <div class="order-2 lg:order-1">
+      <SectionTitle name="Skills" />
+      <p class="opacity-60 text-base lg:text-lg font-light w-full lg:w-[40%] mt-14">
+        {{ skills.description }}
+      </p>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 mt-8">
+      <template v-for="(skill, i) of skills.lists" :key="i">
+        <CardNormal :title="skill.name" :image="skill.image" :sub-title="skill.description" />
+      </template>
+    </div>
   </main>
 </template>
