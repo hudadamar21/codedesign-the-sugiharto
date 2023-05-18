d80def192761843aa49e47ca027cd85434026131
hudadamar21 <hudadamar21@gmail.com>
2023-05-18 14:12:28 +0700

    create card normal component

---
+++ src/components/CardNormal.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  title: String,
+  subTitle: String,
+  image: String,
+})
+</script>
+
+<template>
+  <div class="p-5 pb-7 rounded-lg text-center">
+    <img :src="image" class="aspect-video w-full rounded-lg mb-7" :alt="title" />
+    <h2 class="mb-3">
+      {{ title }}
+    </h2>
+    <p class="text-xs text-white/60 uppercase">{{ subTitle }}</p>
+  </div>
+</template>
