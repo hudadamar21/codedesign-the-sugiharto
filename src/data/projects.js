46e76a2ffcfd62946927e5624e9eafd2f02e5879
hudadamar21 <hudadamar21@gmail.com>
2023-05-18 14:12:53 +0700

    create project data

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+export const projects = {
+  description: 'Di halaman ini kamu akan menemukan proyek-proyek yang pernah saya buat.',
+  categories: ['Semua Kategori', 'UI Design', 'Web Development', 'Mobile Development'],
+  lists: [
+    {
+      name: 'Login Page UI Design',
+      image: '/images/projects/project-1.png',
+      category: 'UI Design'
+    },
+    {
+      name: 'Landing Page',
+      image: '/images/projects/project-2.png',
+      category: 'Web Development'
+    },
+    {
+      name: 'Landing Page',
+      image: '/images/projects/project-3.png',
+      category: 'Web Development'
+    },
+    {
+      name: 'Login Page UI Design',
+      image: '/images/projects/project-4.png',
+      category: 'UI Design'
+    },
+    {
+      name: 'Mobile App',
+      image: '/images/projects/project-5.png',
+      category: 'Mobile Development'
+    },
+    {
+      name: 'Landing Page UI Design',
+      image: '/images/projects/project-6.png',
+      category: 'Web Development'
+    }
+  ]
+}
