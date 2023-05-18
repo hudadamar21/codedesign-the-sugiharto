94998b4e87cab19e3dada309adef46184fe715cf
hudadamar21 <hudadamar21@gmail.com>
2023-05-18 16:52:58 +0700

    change image skill list

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,37 +3,37 @@ export const skills = {
   lists: [
     {
       name: 'Biasa Pake Terminal',
-      image: '/images/projects/project-1.png',
+      image: '/images/skills-1.png',
       description:
         'Udah terbiasa sih pake terimnal kek gini, karena lebih cepet kerjanya ketimbang pake GUI-GUI begitu.'
     },
     {
       name: 'Web Development',
-      image: '/images/projects/project-2.png',
+      image: '/images/skills-2.png',
       description:
         'Bikin web itu gampang, tinggal ketik-ketik kode doang dah jadi lah web. Apalagi sekarang bisa pake Wix hehe.'
     },
     {
       name: 'Nulis Artikel',
-      image: '/images/projects/project-3.png',
+      image: '/images/skills-3.png',
       description:
         'Biasa nulis artikel di web sendiri, ya walaupun masih berantakan yang penting nulis sih, daripada nggak sama sekali.'
     },
     {
       name: 'Biasa Pake Terminal',
-      image: '/images/projects/project-1.png',
+      image: '/images/skills-2.png',
       description:
         'Udah terbiasa sih pake terimnal kek gini, karena lebih cepet kerjanya ketimbang pake GUI-GUI begitu.'
     },
     {
       name: 'Nulis Artikel',
-      image: '/images/projects/project-3.png',
+      image: '/images/skills-3.png',
       description:
         'Biasa nulis artikel di web sendiri, ya walaupun masih berantakan yang penting nulis sih, daripada nggak sama sekali.'
     },
     {
       name: 'Web Development',
-      image: '/images/projects/project-2.png',
+      image: '/images/skills-1.png',
       description:
         'Bikin web itu gampang, tinggal ketik-ketik kode doang dah jadi lah web. Apalagi sekarang bisa pake Wix hehe.'
     }
