e115c077fb8d0c7602bf6a8113993be4d0cdc749
hudadamar21 <hudadamar21@gmail.com>
2023-05-18 14:12:40 +0700

    create section title component

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  name: String
+})
+</script>
+
+<template>
+  <div class="relative mb-7">
+    <h1 class="realtive z-10 text-4xl font-bold">{{ name }}</h1>
+    <div class="absolute -top-9 -left-16 text-[100px] font-bold opacity-[3%] leading-none">
+      {{ name }}
+    </div>
+  </div>
+</template>
