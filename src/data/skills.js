125be920ffe6c991a2cfdc560e9d0fea6ba407c8
hudadamar21 <hudadamar21@gmail.com>
2023-05-18 16:39:20 +0700

    create skills data

---
+++ src/data/skills.js
@@ -0,0 +1,41 @@
+export const skills = {
+  description: 'Beberapa skill yang saya punya, sebenernya banyak skill saya, tapi males masukin.',
+  lists: [
+    {
+      name: 'Biasa Pake Terminal',
+      image: '/images/projects/project-1.png',
+      description:
+        'Udah terbiasa sih pake terimnal kek gini, karena lebih cepet kerjanya ketimbang pake GUI-GUI begitu.'
+    },
+    {
+      name: 'Web Development',
+      image: '/images/projects/project-2.png',
+      description:
+        'Bikin web itu gampang, tinggal ketik-ketik kode doang dah jadi lah web. Apalagi sekarang bisa pake Wix hehe.'
+    },
+    {
+      name: 'Nulis Artikel',
+      image: '/images/projects/project-3.png',
+      description:
+        'Biasa nulis artikel di web sendiri, ya walaupun masih berantakan yang penting nulis sih, daripada nggak sama sekali.'
+    },
+    {
+      name: 'Biasa Pake Terminal',
+      image: '/images/projects/project-1.png',
+      description:
+        'Udah terbiasa sih pake terimnal kek gini, karena lebih cepet kerjanya ketimbang pake GUI-GUI begitu.'
+    },
+    {
+      name: 'Nulis Artikel',
+      image: '/images/projects/project-3.png',
+      description:
+        'Biasa nulis artikel di web sendiri, ya walaupun masih berantakan yang penting nulis sih, daripada nggak sama sekali.'
+    },
+    {
+      name: 'Web Development',
+      image: '/images/projects/project-2.png',
+      description:
+        'Bikin web itu gampang, tinggal ketik-ketik kode doang dah jadi lah web. Apalagi sekarang bisa pake Wix hehe.'
+    }
+  ]
+}
